4a4d224da919ca4baf4f9679ec888912c28a43e1
KMosunov <kmosunov@gmail.com>
2024-12-25 23:24:44 +0300

    HW_05 | 2 options of application making added

--- src/Id_Op1.ts
+++ src/Id_Op1.ts
@@ -13,8 +13,3 @@ export function getRandomLTU (min: number, max:number):number {
 console.log ("EST-" +getRandomEST(1000,10000));
 console.log ("LVA-" +getRandomLVA(1000,10000));
 console.log ("LTU-" +getRandomLTU(1000,10000));
-
-
-//export let x= Math.floor((Math.random() * 9000) + 1000);
-//export let y= Math.floor((Math.random() * 9000) + 1000);
-//export let z= Math.floor((Math.random() * 9000) + 1000);
